3639059888ec42946c4e501a5319a2f249b03df5
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-10-01 10:05:37 -0300

    Instalado módulo ejs. Configurado o arquivo principal para exibir os arquivos de visualização. Adicionado renderização da página de login. Criada página de erro401

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.json());
-app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(express.json())
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 import homeRoute from "./routes/home.js";
 import registerRoute from "./routes/authentication/register.js";
--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -1,13 +1,28 @@
 import express from 'express';
+import userModel from '../../models/user-model.js';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('login');
 });
 
-// Rota para processar o login do usuário
-router.post('/', (req, res) => {
-  // Implemente a lógica para processar o login do usuário
+router.post('/', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user || !user.comparePassword(password)) {
+      return res.status(401).render('error401');
+    }
+
+    res.redirect('/'); 
+
+  } catch (error) {
+    console.error('Erro ao processar o login:', error);
+    res.status(500).render('login', { error: 'Erro interno ao processar o login' });
+  }
 });
 
 export default router;
