4f313d271d9c8ed8fa8f9d76ab50febcbb7d2679
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-10-07 17:13:29 -0300

    Removida do arquivo de login a estratégia temporária de autenticaçã

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import userModel from '../../models/user-model.js';
 import passport from '../../auth/localStrategy-passport.js';
 
 const router = express.Router();
@@ -12,30 +11,6 @@ router.post('/', passport.authenticate('local', { failureRedirect: '/login' }),
 function(req, res) {
   res.redirect('/');
 
-
-/*async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await userModel.findOne({ email });
-
-    const passwordMatch = await userModel.findOne( { password })
-
-    if (!user) {
-      return res.status(401).render('error401');
-    }
-
-    if(!passwordMatch){
-      return res.status(401).render('error401');
-    }
-
-    res.redirect('/'); 
-
-  } catch (error) {
-    console.error('Erro ao processar o login:', error);
-    res.status(500).render('error500');
-  }
-  } */
 });
 
 export default router;
