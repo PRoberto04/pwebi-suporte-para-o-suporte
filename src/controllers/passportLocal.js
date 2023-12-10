76d863b0221b883a1c70306d63de5edf92cf4f7b
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-10 15:21:20 -0300

    Configurada biblioteca Passport para minha aplicação

---
+++ src/controllers/passportLocal.js
@@ -0,0 +1,33 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import userModel from "../models/user-model";
+
+passport.use(new LocalStrategy({usernameField: 'email'},
+    async(email, passport, done) => {
+        try{
+            const foundUser = await userModel.findOne({email: email});
+            if(!foundUser){return done(null, false)};
+
+            const isPasswordValid = await foundUser.verifyPassword(password);
+            if(!isPasswordValid){return done(null, false)};
+            
+            return done(null, foundUser);
+        } catch(error){
+            return done(error);
+        }
+    }
+));
+
+passport.serializeUser(function(userModel, cb){
+    process.nextTick(function() {
+        cb(null, { id: userModel.id, username: userModel.username});
+    });
+});
+
+passport.deserializeUser(function(user, cb){
+    process.nextTick(function(user, cb) {
+        return cb(null, user);
+    });
+});
+
+export default passport;
