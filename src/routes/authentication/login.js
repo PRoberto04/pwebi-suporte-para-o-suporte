a4ced37d53566169c46c56643667d99fad545d1f
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-10 16:39:30 -0300

    Modificada rota de redirecionamento com o login bem sucedido

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/password', passport.authenticate('local', {
-  successReturnToOrRedirect: '/',
+  successReturnToOrRedirect: '/page/home-bolsistas',
   failureRedirect: '/auth/login',
   failureMessage: true
 }));
