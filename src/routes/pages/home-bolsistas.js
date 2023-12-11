622e34828a3b77959224254925594f6e3e2f68b5
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-11 04:40:45 -0300

    Passando usuários para ser usado na lista de usuários na página de visualização

--- src/routes/pages/home-bolsistas.js
+++ src/routes/pages/home-bolsistas.js
@@ -1,10 +1,17 @@
 import express from "express";
 const router = express.Router();
 
-//Rota de acesso à usuários já logados
-//Implementar token de verificação
-router.get('/', (req, res) =>{
-    res.render('home-bolsistas');
+import userModel from "../../models/user-model.js";
+
+router.get('/', async (req, res) =>{
+    try {
+        const users = await userModel.find({}, 'name workShift numberTel');
+
+        res.render('home-bolsistas', {users});
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 export default router;
