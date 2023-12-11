efe04ddcda81f369fa6e954bd00195de915a7881
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-11 10:38:34 -0300

    Adicionadas rotas que gerenciam a autenticação com o google

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -1,23 +1,41 @@
-import express from 'express';
-import passport from '../../controllers/passportLocal.js';
+import express from "express";
+import localPassport from "../../controllers/passportLocal.js";
+import googlePassport from "../../controllers/passportGoogle.js";
 
 const router = express.Router();
 
 //Utiliza o arquivo de visualização, para a página de login
-router.get('/', (req, res) => {
-  res.render('login');
+router.get("/", (req, res) => {
+  res.render("login");
 });
 
-router.post('/password', passport.authenticate('local', {
-  successRedirect: '/page/home-bolsistas',
-  failureRedirect: '/auth/login',
-  failureMessage: true
-}));
+router.get(
+  "/google",
+  googlePassport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.post('/logout', function(req, res, next) {
-  req.logout(function(err) {
-    if(err){ return next(err) };
-    res.redirect('/');
+router.get(
+  "/google/callback",
+  googlePassport.authenticate("google", { failureRedirect: "/" }),
+  function (req, res) {
+    res.redirect("/page/home-bolsistas");
+  }
+);
+
+router.post("/password",
+localPassport.authenticate("local", {
+    successRedirect: "/page/home-bolsistas",
+    failureRedirect: "/auth/login",
+    failureMessage: true,
+  })
+);
+
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
