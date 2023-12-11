a9b22c628da1941684583fb9e25caff1ec4acde4
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-11 12:12:27 -0300

    Criaa função que verifica se o usuário possui uma senha, caso não, redireciona para a página de criação de senha

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -1,8 +1,17 @@
 import express from "express";
+import {createPassword} from "../../controllers/user-controller.js"
 import localPassport from "../../controllers/passportLocal.js";
 import googlePassport from "../../controllers/passportGoogle.js";
 
 const router = express.Router();
+
+const checkPassword = (req, res, next) => {
+  if (req.isAuthenticated() && !req.user.password) {
+    return res.redirect("/auth/login/create-password");
+  }
+
+  return next();
+};
 
 //Utiliza o arquivo de visualização, para a página de login
 router.get("/", (req, res) => {
@@ -17,13 +26,16 @@ router.get(
 router.get(
   "/google/callback",
   googlePassport.authenticate("google", { failureRedirect: "/" }),
+  checkPassword,
   function (req, res) {
     res.redirect("/page/home-bolsistas");
   }
 );
 
-router.post("/password",
-localPassport.authenticate("local", {
+router.post(
+  "/password",
+  checkPassword,
+  localPassport.authenticate("local", {
     successRedirect: "/page/home-bolsistas",
     failureRedirect: "/auth/login",
     failureMessage: true,
@@ -38,5 +50,11 @@ router.post("/logout", function (req, res, next) {
     res.redirect("/");
   });
 });
+
+router.get("/create-password", (req, res) => {
+  res.render("create-pass");
+});
+
+router.post('/create-password', createPassword);
 
 export default router;
