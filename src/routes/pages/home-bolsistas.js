2844d5bb50075f9de6b67c9f8c1c3abab803c93e
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-16 12:58:16 -0300

    Criado middleware que verifica preenchimento dos campos 'workShift' e 'numberTel'

--- src/routes/pages/home-bolsistas.js
+++ src/routes/pages/home-bolsistas.js
@@ -3,7 +3,26 @@ const router = express.Router();
 
 import userModel from "../../models/user-model.js";
 
-router.get('/', async (req, res) =>{
+const checkFieldsMiddleware = async (req, res, next) => {
+    try {
+        if (req.isAuthenticated()) {
+          const user = await userModel.findById(req.user._id);
+          if (user && user.workShift && user.numberTel) {
+            next();
+          } else {
+            return res.redirect('/page/profile/edit');
+          }
+        } else {
+          // Não redirecionar se o usuário não estiver autenticado
+          next();
+        }
+    } catch (error) {
+      console.error('Erro ao verificar campos do usuário', error);
+      res.status(500).render('error/error500');
+    }
+  };
+
+router.get('/', checkFieldsMiddleware, async (req, res) =>{
     try {
         const users = await userModel.find({}, 'name workShift numberTel');
 
