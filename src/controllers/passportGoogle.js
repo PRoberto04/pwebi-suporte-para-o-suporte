467d3173a4e40cbfdd66177cecaa0bc6f0736495
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-16 10:27:36 -0300

    Atualizando estratégia de atenticação, para verificar se o usuário existe, caso sim, o login é feito

--- src/controllers/passportGoogle.js
+++ src/controllers/passportGoogle.js
@@ -5,39 +5,57 @@ import userModel from "../models/user-model.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-passport.use(new GoogleStrategy({
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/login/google/callback'
-}, async function(acessToken, refreshToken, profile, cb){
-        try {
-            let user = await userModel.findOne({googleId: profile.id});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/login/google/callback",
+      scope: ["profile", "email"],
+    },
+    async function (acessToken, refreshToken, profile, cb) {
+      try {
+        let user = await userModel.findOne({ googleId: profile.id });
 
-            if(!user){
-                user = new userModel({
-                    googleId: profile.id,
-                    name: profile.displayName,
-                    email: profile.emails[0].value
-                });
-                await user.save();
+        if (user) {
+          req.login(user, (logiErr) => {
+            if (logiErr) {
+              console.error("Erro ao realizar login: ", logiErr);
             }
-            return cb(null, user);
-        } catch (error) {
-             if (error.code === 11000) {
+            return res.redirect("/page/home-bolsistas");
+          });
+        } else {
+          
+          const existingUser = await userModel.findOne({
+            email: profile.emails[0].value,
+          });
 
-                 return cb(null, false, { message: 'Usuário com este ID do Google já existe.' });
-             }
-            return cb(error);
+          if (existingUser) {
+            user = existingUser;
+          } else{
+              user = new userModel({
+                googleId: profile.id,
+                name: profile.displayName,
+                email: profile.emails[0].value,
+              });
+              await user.save();
+          }
+
+          return cb(null, user);
         }
+      } catch (error) {
+        return cb(error);
+      }
     }
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 });
 
-passport.deserializeUser((obj, done) =>{
-    done(null, obj);
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
 });
 
 export default passport;
