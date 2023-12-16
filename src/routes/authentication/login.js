3f1b384c121528028d6ac012c5a34cda97c2d5bc
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-16 07:37:29 -0300

    Removendo estratégia de autenticação local

--- src/routes/authentication/login.js
+++ src/routes/authentication/login.js
@@ -1,6 +1,4 @@
 import express from "express";
-import {createPassword} from "../../controllers/user-controller.js"
-import localPassport from "../../controllers/passportLocal.js";
 import googlePassport from "../../controllers/passportGoogle.js";
 
 const router = express.Router();
@@ -32,16 +30,6 @@ router.get(
   }
 );
 
-router.post(
-  "/password",
-  checkPassword,
-  localPassport.authenticate("local", {
-    successRedirect: "/page/home-bolsistas",
-    failureRedirect: "/auth/login",
-    failureMessage: true,
-  })
-);
-
 router.post("/logout", function (req, res, next) {
   req.logout(function (err) {
     if (err) {
@@ -50,11 +38,5 @@ router.post("/logout", function (req, res, next) {
     res.redirect("/");
   });
 });
-
-router.get("/create-password", (req, res) => {
-  res.render("create-pass");
-});
-
-router.post('/create-password', createPassword);
 
 export default router;
