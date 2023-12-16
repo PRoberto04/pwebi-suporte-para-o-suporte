7d629cf9a84cb8c66598d1d7fd334d353cbee178
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-16 07:42:15 -0300

    Removendo rota POST para criação de usuário

--- src/routes/authentication/register.js
+++ src/routes/authentication/register.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import { createUser } from '../../controllers/user-controller.js';
 const router = express.Router();
 
 //Rota de registro de usuário
 router.get('/', (req, res) => {
   res.render('register');
 });
-
-//Lógica básica para registro de usuário
-router.post('/', createUser);
 
 export default router;
