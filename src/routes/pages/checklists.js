01adb52f577994701050aac9153db44922039bf4
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-17 12:24:22 -0300

    Adicionandos controladores de rota que verificarão o status do checklist e passando usuário como paarâmetro

--- src/routes/pages/checklists.js
+++ src/routes/pages/checklists.js
@@ -1,9 +1,18 @@
 import express from "express";
+import {updateChecklistStatus, verifyCheckList} from "../../controllers/user-controller.js"
 const router = express.Router();
 
 //Rota para acesso à funcionalidade checklist 
-router.get('/', (req, res) =>{
-    res.render('checklists')
+router.get('/', async (req, res) =>{
+    try {
+        await verifyCheckList(req, res);
+        res.render('checklists', {user: req.user});
+        
+    } catch (error) {
+        console.error('Erro ao renderizar a página do checklist:', error);
+    }
 });
+
+router.post('/submit', updateChecklistStatus);
 
 export default router;
