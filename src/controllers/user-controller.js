4efdc72d245d97abba53f024bbe11db90f75ad3d
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-17 12:22:09 -0300

    Adicionando novos métodos, para atualizar checklist de usuário e verificar se o checklist está completo

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,7 +3,6 @@ import userModel from '../models/user-model.js';
 export const updateProfile = async (req, res) =>{
     try {
         const {workShift, numberTel, options} = req.body;
-
         const user = await userModel.findByIdAndUpdate(
             req.user._id,
             {workShift, numberTel, days: options || []},
@@ -19,3 +18,51 @@ export const updateProfile = async (req, res) =>{
     }
 }
 
+export const verifyCheckList = async (req, res) => {
+    try {
+        const user = await userModel.findById(req.user._id);
+        if(!user){
+            throw new Error('Usuário não encontrado');
+        }
+        const isChecklistComplete = user.checklist.every(item => item.checked);
+        user.checklistComplete = isChecklistComplete;
+        await user.save();
+    } catch (error) {
+        console.error('Erro: ', error)
+    }
+}
+
+export const updateChecklistStatus = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+
+        if (!user.checklist || !Array.isArray(user.checklist)) {
+            return res.status(404).json({ error: 'Checklist não encontrado' });
+        }
+
+        console.log(Array.isArray(user.checklist)); 
+        console.log(user.checklist);
+
+        if (!req.body.checklist || !Array.isArray(req.body.checklist) || !req.body.checklist.every(item => item.hasOwnProperty('label') && item.hasOwnProperty('checked'))) {
+            return res.status(400).json({ error: 'Dados do checklist inválidos' });
+        }
+        
+        user.checklist = req.body.checklist.map(item => ({
+            label: item.label,
+            checked: item.checked === 'true'
+        }));
+        
+        user.checklistComplete = user.checklist.every(item => item.checked);
+        await user.save();
+
+        res.redirect('../home-bolsistas');
+    } catch (error) {
+        console.error('Erro ao atualizar status de checklistComplete:', error);
+        res.status(500).render('error/error500');
+    }
+};
