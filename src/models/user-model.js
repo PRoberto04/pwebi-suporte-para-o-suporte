e3fe271e0ff821395b191137b87981fa8dc77ed3
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-17 12:20:30 -0300

    Adicionando checklist padrão ao modelo de usuário, adicionando atributos de checklist e checklistComplete

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -10,9 +10,33 @@ const userSchema = new Schema({
     workShift: {type: String},
     numberTel: {type: String},
     days: { type: [String], default: [] },
-    // achievementChecklist: { type: mongoose.Schema.Types.ObjectId, ref: 'checknewuser'},
-    // computerSetupChecklist: { type: mongoose.Schema.Types.ObjectId, ref: 'checkformatting'},
+    checklist:  {type: [{ label: String, checked: {type: Boolean, default: false}}], default: []},
+    checklistComplete: {type: Boolean, default: false}
 });
+
+userSchema.statics.createDefaultChecklist = async function (userId) {
+    try {
+        const user = await this.findById(userId);
+
+        if (!user) {
+            console.log('Usuário não encontrado');
+            return;
+        }
+
+        user.checklist.push(
+            { label: 'Recebeu fardamento do Serviço Social?', checked: false },
+            { label: 'Recebeu a chave do armário?', checked: false },
+            { label: 'Leu as normas do setor?', checked: false },
+            { label: 'Definiu sua escala de atuação?', checked: false },
+            { label: 'Já conheceu as bancas e ferramentas?', checked: false },
+            { label: 'Começou a ler alguma apostila?', checked: false }   
+        );
+
+        await user.save();
+    } catch (error) {
+        console.error('Erro ao criar checklist padrão:', error);
+    }
+};
 
 const userModel = mongoose.model('userModel', userSchema);
 
