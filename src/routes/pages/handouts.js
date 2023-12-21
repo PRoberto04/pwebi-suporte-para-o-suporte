17a723ec91f513594d773b2b3777a63ddf0b40f2
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-21 16:06:41 -0300

    Modificados nomes de arquivos referentes as apostilas 'handouts' -> 'apostilas'

--- src/routes/pages/handouts.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-//Rota para acesso às apostilas
-router.get('/', (req, res) =>{
-    res.render('handouts')
-});
-
-export default router;
