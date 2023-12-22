37e31ba808ca4b97600bab9808506c7ff570e667
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-22 05:05:11 -0300

    Adaptando à nova configuração do multer

--- src/routes/pages/apostilas.js
+++ src/routes/pages/apostilas.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { createApostila, getApostilas } from "../../controllers/apostila-controller.js";
-import upload from "../../../config/multer.js"
+import {uploadApostila} from "../../../config/multer.js"
 const router = express.Router();
 
 //Rota responsável pela apresentação das apostilas
 router.get('/', getApostilas);
 
-//Rotas responsáveis pelaa página de "criação" de apostilas
+//Rotas responsáveis pela página de "criação" de apostilas
 router.get('/create', (req, res) =>{
-    res.render('apostilas-create')
+    res.render('apostilas-create');
 });
 
-router.post('/upload', upload.single('file'), createApostila)
+router.post('/upload', uploadApostila.single('file'), createApostila);
 
 export default router;
