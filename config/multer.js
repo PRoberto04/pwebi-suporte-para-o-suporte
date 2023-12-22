3ad6b5d72ada6c0862921768c6bae5996124a633
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-22 05:03:56 -0300

    Modificando multer para armazenar em outra pasta

--- config/multer.js
+++ config/multer.js
@@ -1,9 +1,9 @@
-import path from 'path';
 import multer from 'multer';
+import path from 'path';
 
-const storage = multer.diskStorage({
+const storage = (folder) => multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/apostilas')
+    cb(null, `public/uploads/${folder}`)
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
@@ -11,6 +11,5 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage })
-
-export default upload;
+export const uploadApostila = multer({ storage: storage('apostilas') });
+export const uploadTutorial = multer({ storage: storage('tutorials') });
