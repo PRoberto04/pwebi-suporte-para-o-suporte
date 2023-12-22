fa17d698f51b5327b2b27c8ed1c4cdc5ebbea82d
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-22 09:59:53 -0300

    Adaptando página home-bolsistas para receber regras do banco de dados

--- src/routes/pages/home-bolsistas.js
+++ src/routes/pages/home-bolsistas.js
@@ -2,6 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import userModel from "../../models/user-model.js";
+import rulesModel from "../../models/rules-model.js";
+
+import {createRule} from "../../controllers/rules-controller.js"
 
 const checkFieldsMiddleware = async (req, res, next) => {
     try {
@@ -24,12 +27,19 @@ const checkFieldsMiddleware = async (req, res, next) => {
 router.get('/', checkFieldsMiddleware, async (req, res) =>{
     try {
         const users = await userModel.find({}, 'name workShift numberTel');
+        const rules = await rulesModel.find({});
 
-        res.render('home-bolsistas', {users});
+        res.render('home-bolsistas', {users, rules});
     } catch (error) {
         console.error('Erro ao buscar dados do usuário:', error);
         res.status(500).render('error/error500');
     }
 });
+
+router.get('/create-rules', (req, res)=>{
+  res.render('rules-create')
+});
+
+router.post('/upload', createRule);
 
 export default router;
